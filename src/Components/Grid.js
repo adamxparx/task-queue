c2514635912287601c20285269d955286b545692
LAB14_USER ON WS32 <L14X08W32@LabHCI.net>
2025-09-24 10:31:55 +0800

    Updated task queue button onclicks

--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -1,13 +1,34 @@
 import Grid from '@mui/material/Grid';
+import { useState } from 'react';
 
 export default function QueueGrid() {
+
+    const [randomNumber, setRandomNumber] = useState(null);
+    const [tasks, setTasks] = useState([]);
+
+    const generateRandomNumber = () => {
+        const min = 50;
+        const max = 300;
+        const newRandom = Math.floor(Math.random() * (max - min + 1)) + min;
+        setRandomNumber(newRandom);
+    }
+
+    const handleOnclick = () => {
+        generateRandomNumber();
+        setTasks([...tasks, randomNumber])
+    }
+
     return (
         <>
             <Grid container spacing={2} sx={{ border: "1px solid black", width: "1000px", height: "auto", padding: "20px"}}>
                 <Grid item size={7} sx={{ border: "2px solid rgba(0, 0, 0, 0.3)", height: "800px" }}>
-                    <button className="AddTask">Add Random Task</button>
+                    <button onClick={handleOnclick} className="AddTask">Add Random Task</button>
                     <h2 className="TaskQueue">Task Queue</h2>
-                    <div className="RandomTask" />
+                    <div className="RandomTask">
+                        {tasks.map((task, i) => (
+                            <li>{task}</li>
+                        ))}
+                    </div>
                     <button className="AdmitTask">Admit Task</button>
                 </Grid>
                 <Grid item container size={5} spacing={2} sx={{ maxHeight: "800px" }}>
