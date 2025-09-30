0500299af5d127d2b759426013f8a35414b8319c
Adam Cloyd Esparcia <adamcloydesparcia@gmail.com>
2025-09-30 23:09:29 +0800

    Added usestate task lists

--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -1,64 +1,91 @@
 import Grid from '@mui/material/Grid';
+import { useEffect } from 'react';
 import { useState } from 'react';
 
 export default function QueueGrid() {
+    
+    const min = 50;
+    const max = 300;
 
-    const [randomNumber, setRandomNumber] = useState(null);
+    const initialRandom = Math.floor(Math.random() * (max - min + 1)) + min;
+    const [randomNumber, setRandomNumber] = useState(initialRandom);
+
+    const [timer, setTimer] = useState(0);
+    const [count, setCount] = useState(12);
+
     const [tasks, setTasks] = useState([]);
+    const [highPrio, setHighPrio] = useState([]);
 
     const generateRandomNumber = () => {
-        const min = 50;
-        const max = 300;
         const newRandom = Math.floor(Math.random() * (max - min + 1)) + min;
         setRandomNumber(newRandom);
     }
 
-    const handleOnclick = () => {
+    const handleOnclickAdd = () => {
         generateRandomNumber();
-        setTasks([...tasks, randomNumber])
+        setTasks([...tasks, randomNumber]);
     }
+
+    const handleOnclickAdmit = () => {
+        if (tasks[0] != null) {
+            const firstNum = tasks.shift();
+            setHighPrio([...highPrio, firstNum]);
+
+            setTimer(() => {
+                return 12 - (12 / firstNum);
+            });
+        }
+    }
+
+    // useEffect(() => {
+    //     console.log(timer);
+    // }, )
 
     return (
         <>
             <Grid container spacing={2} sx={{ border: "1px solid black", width: "1000px", height: "auto", padding: "20px"}}>
                 <Grid item size={7} sx={{ border: "2px solid rgba(0, 0, 0, 0.3)", height: "800px" }}>
-                    <button onClick={handleOnclick} className="AddTask">Add Random Task</button>
+                    <button onClick={handleOnclickAdd} className="AddTask">Add Random Task</button>
                     <h2 className="TaskQueue">Task Queue</h2>
                     <div className="RandomTask">
                         {tasks.map((task, i) => (
                             <li>{task}</li>
                         ))}
                     </div>
-                    <button className="AdmitTask">Admit Task</button>
+                    <button onClick={handleOnclickAdmit} className="AdmitTask">Admit Task</button>
                 </Grid>
                 <Grid item container size={5} spacing={2} sx={{ maxHeight: "800px" }}>
                     <Grid item size={12} sx={{ border: "2px solid red", padding: "0px 5px" }}>
                         <h3>High PriorityQueue 1</h3>
                         <p>Queue List:</p>
-                        <div className="QueueList" />
+                        <Grid className="QueueList">
+                            {highPrio.map((task, i) => (
+                                <li>{task}</li>
+                            ))}
+                        </Grid>
                         <p>Duration:</p>
-                        <div className="Duration" />
+                        <Grid item size={count} className="Duration" />
                     </Grid>
                     <Grid item size={12} sx={{ border: "2px solid rgba(0, 0, 0, 0.3)", padding: "0px 5px" }}>
-                        <h3>Regular Task 2</h3>
+                        <h3>High PriorityQueue 1</h3>
                         <p>Queue List:</p>
-                        <div className="QueueList" />
+                        <Grid className="QueueList" />
                         <p>Duration:</p>
-                        <div className="Duration" />
+                        <Grid className="Duration" />
                     </Grid>
                     <Grid item size={12} sx={{ border: "2px solid rgba(0, 0, 0, 0.3)", padding: "0px 5px" }}>
-                        <h3>Regular Task 3</h3>
+                        <h3>High PriorityQueue 1</h3>
                         <p>Queue List:</p>
-                        <div className="QueueList" />
+                        <Grid className="QueueList" />
                         <p>Duration:</p>
-                        <div className="Duration" />
+                        <Grid className="Duration" />
                     </Grid>
                     <Grid item size={12} sx={{ border: "2px solid rgba(0, 0, 0, 0.3)", padding: "0px 5px" }}>
-                        <h3>Regular Task 4</h3>
+                        <h3>High PriorityQueue 1</h3>
                         <p>Queue List:</p>
-                        <div className="QueueList" />
+                        <Grid className="QueueList" />
                         <p>Duration:</p>
-                        <div className="Duration" />
+                        <Grid className="Duration" />
                     </Grid>
                 </Grid>
             </Grid>
